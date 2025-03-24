03bd8c7ac13d624cbb7af8ccc6bd015021678efe
Menurva <gngo880@gmail.com>
2025-03-25 03:40:35 +0700

    adding the rope mechanics

---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public Transform cameraposition;
+    private void Update()
+    {
+        transform.position = cameraposition.position;
+    }
+}
